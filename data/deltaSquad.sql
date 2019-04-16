4712237178decc90ecf3b4a6f9ab5cfe6884b6d0
adamRajmuller <rajfta@gmail.com>
2019-04-16 11:20:13 +0200

    sql

---
+++ data/deltaSquad.sql
@@ -0,0 +1,84 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.6
+-- Dumped by pg_dump version 9.5.6
+
+-- ALTER TABLE IF EXISTS ONLY public.question DROP CONSTRAINT IF EXISTS pk_question_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.boards DROP CONSTRAINT IF EXISTS pk_boards_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS fk_card_status_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS fk_card_board_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS pk_cards_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.statuses DROP CONSTRAINT IF EXISTS pk_statuses_id CASCADE;
+
+
+
+
+DROP TABLE IF EXISTS public.boards;
+DROP SEQUENCE IF EXISTS public.boards_id_seq;
+CREATE TABLE boards (
+    id serial NOT NULL,
+    title text
+);
+
+DROP TABLE IF EXISTS public.cards;
+DROP SEQUENCE IF EXISTS public.cards_id_seq;
+CREATE TABLE cards (
+    id serial NOT NULL,
+    board_id integer,
+    title text,
+    status_id integer,
+    "order" integer
+);
+
+
+DROP TABLE IF EXISTS public.statuses;
+DROP SEQUENCE IF EXISTS public.statuses_id_seq;
+CREATE TABLE statuses (
+    id serial NOT NULL,
+    title text
+);
+
+
+ALTER TABLE ONLY statuses
+    ADD CONSTRAINT pk_statuses_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY boards
+    ADD CONSTRAINT pk_boards_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY cards
+    ADD CONSTRAINT pk_cards_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY cards
+    ADD CONSTRAINT fk_card_board_id FOREIGN KEY (board_id) REFERENCES boards(id);
+
+ALTER TABLE ONLY cards
+    ADD CONSTRAINT fk_card_status_id FOREIGN KEY (status_id) REFERENCES statuses(id);
+
+
+INSERT INTO boards VALUES (2, 'Board 1');
+INSERT INTO boards VALUES (1, 'Board 2');
+
+
+INSERT INTO statuses VALUES (0, 'new');
+INSERT INTO statuses VALUES (1, 'in progress');
+INSERT INTO statuses VALUES (2, 'testing');
+INSERT INTO statuses VALUES (3, 'done');
+
+
+INSERT INTO cards VALUES (1, 1, 'new card', 0, 0);
+INSERT INTO cards VALUES (2, 1, 'new card', 0, 1);
+INSERT INTO cards VALUES (3, 1, 'in progress card', 1, 0);
+INSERT INTO cards VALUES (4, 2, 'planning', 2, 0);
+INSERT INTO cards VALUES (5, 2, 'done card', 3, 0);
+INSERT INTO cards VALUES (6, 2, 'done card', 3, 1);
+
+
+
+
+SELECT pg_catalog.setval('statuses_id_seq', 1, true);
+
+SELECT pg_catalog.setval('boards_id_seq', 2, true);
+
+SELECT pg_catalog.setval('cards_id_seq', 2, true);
